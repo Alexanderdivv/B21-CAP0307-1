4ec93154a92fc3e0df947560544b15c5bb060dc6
Alexanderdivv <alexanderdivagraelbangun@gmail.com>
2021-06-08 11:49:59 +0700

    delete unusable for deployment file

--- Android Path/FatigueApplication/app/src/main/java/com/project1/fatigueapplication/data/api/MLRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.project1.fatigueapplication.data.api
-
-import com.google.gson.annotations.SerializedName
-
-data class MLRequest(
-
-    @field:SerializedName("gambar")
-    val image: String,
-)
--- Android Path/FatigueApplication/app/src/main/java/com/project1/fatigueapplication/data/viewdata/IntroSlider.kt
+++
@@ -1,7 +0,0 @@
-package com.project1.fatigueapplication.data.viewdata
-
-data class IntroSlider(
-    val title: String,
-    val description: String,
-    val icon: Int
-)
